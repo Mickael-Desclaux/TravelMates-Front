3a98252786027a9d4807f3ea9c46b596c1706c44
EstherVlp <35601411+EstherVlp@users.noreply.github.com>
2024-10-09 18:43:15 +0200

    [TRA-6] added and updated the form with the library Yup

--- travel-mates/src/pages/Auth/SignUp.tsx
+++ travel-mates/src/pages/Auth/SignUp.tsx
@@ -1,127 +1,174 @@
-import { Typography, Button } from "@material-tailwind/react";
-import { ErrorMessage, Field, Formik } from 'formik';
+import { Typography, Button, Progress } from "@material-tailwind/react";
+import { ErrorMessage, Field, Formik } from "formik";
+import * as Yup from "yup";
+//import travelMatesLogo from "../../assets/Logo/travelmates.png";
 
-/* interface SignUpProps {
-    email: string,
-    password: string,
-    confirmedPassword: string
-} */
 export default function SignUp() {
-
+  const SignupSchema = Yup.object().shape({
+    email: Yup.string()
+      .default("red")
+      .email("Email invalide")
+      .required("L'email est requis"),
+    password: Yup.string()
+      .min(8, "Le mot de passe doit contenir au moins 8 caractères")
+      .required("Le mot de passe est requis"),
+    confirmedPassword: Yup.string()
+      .oneOf(
+        [Yup.ref("password"), null],
+        "Les mots de passe doivent correspondre"
+      )
+      .required("La confirmation du mot de passe est requise"),
+  });
 
   return (
-    <section className="grid text-center h-screen items-center p-8">
-      <div>
-        <Typography variant="h3" color="blue-gray" className="mb-2">
-          Créer un compte
-        </Typography>
+    <>
+      <section className="grid text-center h-screen items-center p-8">
+        <div>
+          {/* TravelMates logo 
+          <div className="flex justify-center mb-8">
+            <img
+              src={travelMatesLogo}
+              alt="Logo TravelMates"
+              className="w-40"
+            />
+          </div> */}
 
-        <Formik
-            initialValues={{ email: '', password: '', confirmedPassword: '' }}
-            validate={values => {
-            const errors = {};
-            if (!values.email) {
-            errors.email = 'Required';
-            } else if (
-            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-            ) {
-            errors.email = 'Email invalide';
-            }
-            if (!values.password) {
-                errors.password = 'Required';
+          <Typography variant="h3" color="blue-gray" className="mb-4">
+            Créer un compte
+          </Typography>
+
+          {/* Progress bar */}
+          <div>
+            <Progress value={33} />
+          </div>
+
+          {/* Sign up form */}
+          <Formik
+            initialValues={{ email: "", password: "", confirmedPassword: "" }}
+            validationSchema={SignupSchema}
+            validate={(values) => {
+              const errors = {};
+
+              // Verified if all fields are empty
+              if (
+                !values.email &&
+                !values.password &&
+                !values.confirmedPassword
+              ) {
+                errors.general = "Tous les champs sont requis";
               }
-              if (!values.confirmedPassword) {
-                errors.confirmedPassword = 'Required';
-              } else if (values.password !== values.confirmedPassword) {
-                errors.confirmedPassword = 'Les mots de passe ne correspondent pas';
-              }
-            return errors;
+
+              return errors;
             }}
             onSubmit={(values, { setSubmitting }) => {
-            setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-            }, 400);
-        }}
-        >
-        {({ isSubmitting }) => (
-            <form action="#" className="mx-auto mt-16 max-w-[24rem] text-left" onSubmit={(e) => e.preventDefault()} >
-                <div className="mb-6">
+              setTimeout(() => {
+                alert(JSON.stringify(values, null, 2));
+                setSubmitting(false);
+              }, 400);
+            }}
+          >
+            {({ errors, isSubmitting }) => (
+              <form
+                action="#"
+                className="mx-auto mt-16 max-w-[24rem] text-left"
+              >
+                {/* Display of general errors */}
+                {errors.general && (
+                  <div className="text-red-500 mb-4">{errors.general}</div>
+                )}
 
-                    <label htmlFor="email">
+                <div className="mb-6">
+                  <label htmlFor="email">
                     <Typography
-                        variant="small"
-                        className="mb-2 block font-medium text-gray-900"
+                      variant="small"
+                      className="mb-2 block font-medium text-gray-900"
                     >
-                        {/* {email} */}
-                        Email
+                      Email
                     </Typography>
-                    </label>
-                    <Field
+                  </label>
+                  <Field
                     id="email"
                     color="gray"
                     type="email"
                     name="email"
                     placeholder="Votre email"
                     className="w-full mb-2 bg-transparent placeholder:text-slate-400 text-gray-900 text-sm border border-gray-400 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300"
-                    />
-                    <ErrorMessage name="email" component="div" />
+                  />
+                  <ErrorMessage
+                    name="email"
+                    component="div"
+                    className="text-red-500"
+                  />
                 </div>
                 <div className="mb-6">
-                    <label htmlFor="password">
+                  <label htmlFor="password">
                     <Typography
-                        variant="small"
-                        className="mb-2 block font-medium text-gray-900"
+                      variant="small"
+                      className="mb-2 block font-medium text-gray-900"
                     >
-                        {/* {password} */}
-                        Mot de passe
+                      Mot de passe
                     </Typography>
-                    </label>
-                    <Field
+                  </label>
+                  <Field
                     id="password"
                     type="password"
                     name="password"
-                    placeholder="********"
+                    placeholder="Choisir un mot de passe"
                     className="w-full mb-2 bg-transparent placeholder:text-slate-400 text-gray-900 text-sm border border-gray-400 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300"
-                    />
-                    <ErrorMessage name="password" component="div" />
+                  />
+                  <ErrorMessage
+                    name="password"
+                    component="div"
+                    className="text-red-500"
+                  />
                 </div>
                 <div className="mb-6">
-                    <label htmlFor="password">
+                  <label htmlFor="password">
                     <Typography
-                        variant="small"
-                        className="mb-2 block font-medium text-gray-900"
+                      variant="small"
+                      className="mb-2 block font-medium text-gray-900"
                     >
-                        {/* {confirmedPassword} */}
-                        Confirmer votre mot de passe
+                      Confirmer votre mot de passe
                     </Typography>
-                    </label>
-                    <Field
+                  </label>
+                  <Field
                     id="confirmedPassword"
                     type="password"
                     name="confirmedPassword"
-                    placeholder="********"
+                    placeholder="Confirmer votre mot de passe"
                     className="w-full mb-2 bg-transparent placeholder:text-slate-400 text-gray-900 text-sm border border-gray-400 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300"
-                    />
-                    <ErrorMessage name="password" component="div" />
+                  />
+                  <ErrorMessage
+                    name="confirmedPassword"
+                    component="div"
+                    className="text-red-500"
+                  />
                 </div>
-                <Button type="submit" disabled={isSubmitting} color="gray" size="lg" className="mt-6" fullWidth>
-                    Continuer
+                <Button
+                  type="submit"
+                  disabled={isSubmitting}
+                  color="gray"
+                  size="lg"
+                  className="mt-6"
+                  fullWidth
+                >
+                  Continuer
                 </Button>
                 <Typography
-                    variant="small"
-                    color="gray"
-                    className="!mt-4 text-center font-normal"
+                  variant="small"
+                  color="gray"
+                  className="!mt-4 text-center font-normal"
                 >
-                    Vous avez déjà un compte ?{" "}
-                    <a href="#" className="font-medium text-gray-900">
+                  Vous avez déjà un compte ?{" "}
+                  <a href="/SignIn" className="font-medium text-gray-900 underline">
                     Me connecter
-                    </a>
+                  </a>
                 </Typography>
-            </form>
+              </form>
             )}
-        </Formik>
-      </div>
-    </section>
+          </Formik>
+        </div>
+      </section>
+    </>
   );
 }
