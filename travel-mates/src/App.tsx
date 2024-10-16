87dd4f2e97b54cde0c3b602da3274429df58f2e0
Tom Tissot <tissot.tom31@gmail.com>
2024-10-16 11:35:40 +0200

    [TRA-49] Added import of TripCreate and route /trip-create in App.tsx

--- travel-mates/src/App.tsx
+++ travel-mates/src/App.tsx
@@ -1,21 +1,21 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import TripListe from './pages/Trip/TripList';
-import SignIn from './pages/Auth/SignIn'
-import SignUp from './pages/Auth/SignUp'
+import SignIn from './pages/Auth/SignIn';
+import SignUp from './pages/Auth/SignUp';
+import TripCreate from './pages/Trip/TripCreate';
 
 function App() {
-
-  return (
-    <>
-      <Routes>
-      	<Route path="/" element={<TripListe />} />
-        <Route path='/sign-in' element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        
-      </Routes>
-    </>
-  )
+	return (
+		<>
+			<Routes>
+				<Route path="/" element={<TripListe />} />
+				<Route path="/sign-in" element={<SignIn />} />
+				<Route path="/sign-up" element={<SignUp />} />
+				<Route path="/trip-create" element={<TripCreate />} />
+			</Routes>
+		</>
+	);
 }
 
 export default App;
