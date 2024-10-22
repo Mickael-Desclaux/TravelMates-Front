f0545d0b787077711a194d38016c29c15e9fddf5
Mickael <mickaeldesclaux@gmail.com>
2024-10-22 15:46:22 +0200

    [TRA-205] Add pin pop over in Map.tsx

--- travel-mates/src/pages/Map/Map.tsx
+++ travel-mates/src/pages/Map/Map.tsx
@@ -1,21 +1,69 @@
 import mapboxgl from 'mapbox-gl/dist/mapbox-gl'
 import { useEffect } from 'react';
 import './Map.css';
+import { Activity } from '../../interfaces/TripProps/TripProps';
+import adventureIcon from '../../assets/activity/adventure.svg';
+import cultureIcon from '../../assets/activity/culture.svg';
+import relaxationIcon from '../../assets/activity/relaxation.svg';
+import sportIcon from '../../assets/activity/sport.svg';
+import partyIcon from '../../assets/activity/party-and-bar.svg';
+import pinMarker from '../../assets/icons/pin-marker.svg';
+import { renderToString } from 'react-dom/server';
+import { Button, Typography } from '@material-tailwind/react';
 
 interface Pin {
     title: string;
+    activities: Activity[];
     latitude: number;
     longitude: number;
+    rating: number
 }
 
 export default function Map() {
 
+    // Fake data
     const data: Pin[] = [
-        { title: 'Tour Eiffel', latitude: 48.8584, longitude: 2.2945 },
-        { title: 'Musée du Louvre', latitude: 48.8606, longitude: 2.3376 },
-        { title: 'Cathédrale Notre-Dame', latitude: 48.8529, longitude: 2.3500 },
-        { title: 'Arc de Triomphe', latitude: 48.8738, longitude: 2.2950 },
-        { title: 'Basilique du Sacré-Cœur', latitude: 48.8867, longitude: 2.3431 }
+        {
+            title: 'Tour Eiffel',
+            activities: [
+                { id: 1, type: 'Museum', icon: cultureIcon }, 
+                { id: 2, type: 'Adventure', icon: adventureIcon },
+                { id: 3, type: 'Détente', icon: relaxationIcon },
+                { id: 5, type: 'Fête', icon: partyIcon },
+                { id: 4, type: 'Sport', icon: sportIcon }],
+
+            latitude: 48.8584,
+            longitude: 2.2945,
+            rating: 4
+        },
+        {
+            title: 'Musée du Louvre',
+            activities: [{ id: 1, type: 'Museum', icon: cultureIcon }, { id: 2, type: 'Adventure', icon: adventureIcon }],
+            latitude: 48.8606,
+            longitude: 2.3376,
+            rating: 4
+        },
+        {
+            title: 'Cathédrale Notre-Dame',
+            activities: [{ id: 1, type: 'Museum', icon: cultureIcon }, { id: 2, type: 'Adventure', icon: adventureIcon }],
+            latitude: 48.8529,
+            longitude: 2.3500,
+            rating: 4
+        },
+        {
+            title: 'Arc de Triomphe',
+            activities: [{ id: 1, type: 'Museum', icon: cultureIcon }, { id: 2, type: 'Adventure', icon: adventureIcon }],
+            latitude: 48.8738,
+            longitude: 2.2950,
+            rating: 4
+        },
+        {
+            title: 'Basilique du Sacré-Cœur',
+            activities: [{ id: 1, type: 'Museum', icon: cultureIcon }, { id: 2, type: 'Adventure', icon: adventureIcon }],
+            latitude: 48.8867,
+            longitude: 2.3431,
+            rating: 4
+        }
     ];
 
     mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_API_KEY;
@@ -35,16 +83,51 @@ export default function Map() {
         // Ajoute des contrôles de navigation
         map.addControl(new mapboxgl.NavigationControl());
 
-        data.forEach(function(markerData) {
-            const el = document.createElement('div');
-            el.className='marker';
+        const markers: mapboxgl.Marker[] = [];
 
-            new mapboxgl.Marker(el, { anchor: 'center' })
-            .setLngLat([markerData.longitude, markerData.latitude])
-            .setPopup(new mapboxgl.Popup({ offset: 25 }) // Ajoutez des popups
-            .setHTML('<h3>' + markerData.title + '</h3>'))
-            .addTo(map);
+        data.forEach(function (markerData) {
+            const el = document.createElement('div');
+            el.innerHTML = `<img src="${pinMarker}" width="36" height="72">`;
+
+            const popup = new mapboxgl.Popup()
+                .setHTML(renderToString(
+                    <div>
+                        <Typography variant='h2' className='text-lg mt-2 mb-4'>{markerData.title}</Typography>
+                        <div className='flex felx-row gap-2 mb-6'>
+                            {markerData.activities.map(activity => (
+                                <img
+                                    key={activity.id}
+                                    src={activity.icon}
+                                    alt={activity.type}
+                                    className="h-9 w-9"
+                                />
+                            ))}
+                        </div>
+                        <div className='flex justify-center'>
+                            <Button size='sm' className='bg-black'>Voir plus</Button>
+                        </div>
+                    </div>
+                ));
+
+            const marker = new mapboxgl.Marker(el, { anchor: 'center' })
+                .setLngLat([markerData.longitude, markerData.latitude])
+                .setPopup(popup);
+            markers.push(marker);
         })
+
+        map.on('zoomend', function() {
+
+            const zoom = map.getZoom();
+    
+            markers.forEach(function(marker) {
+                if (zoom >= 4) {
+                    marker.addTo(map);
+                } 
+                else {
+                    marker.remove();
+                }
+            });
+        });
 
         return () => map.remove();
     }, []);
