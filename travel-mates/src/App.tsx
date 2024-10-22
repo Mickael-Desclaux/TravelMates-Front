25ea23052cec429701f0f346c1af0d089a3e5749
Tom Tissot <tissot.tom31@gmail.com>
2024-10-22 11:03:00 +0200

    App.tsx fix

--- travel-mates/src/App.tsx
+++ travel-mates/src/App.tsx
@@ -4,7 +4,7 @@ import TripListe from './pages/Trip/TripList';
 import SignIn from './pages/Auth/SignIn';
 import SignUpMultiStepForm from './pages/Auth/SignUp';
 import TripCreate from './pages/Trip/TripCreate';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 function App() {
 	return (
@@ -12,12 +12,13 @@ function App() {
 			<Routes>
 				<Route path="/" element={<TripListe />} />
 				<Route path="/sign-in" element={<SignIn />} />
-	      <Route path="/sign-up" element={<SignUpMultiStepForm />} />
+				<Route path="/sign-up" element={<SignUpMultiStepForm />} />
 				<Route path="/trip-create" element={<TripCreate />} />
 			</Routes>
 
-      <ReactQueryDevtools/>
+			<ReactQueryDevtools />
 		</>
 	);
+}
 
 export default App;
