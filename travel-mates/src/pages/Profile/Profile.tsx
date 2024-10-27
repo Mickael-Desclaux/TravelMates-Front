df00b810bcfe6bac84cb130477a27976ccd83fd1
EstherVlp <35601411+EstherVlp@users.noreply.github.com>
2024-10-27 22:09:52 +0100

    [TRA-253] added statistics data of users and biography section

--- travel-mates/src/pages/Profile/Profile.tsx
+++ travel-mates/src/pages/Profile/Profile.tsx
@@ -5,6 +5,7 @@ import ProfilePicture from "../../assets/profile/profil1.jpg";
 
 export default function Profile() {
 
+  // Fake profile data
   const profile: ProfileData = {
       id: 1,
       profilePicture: ProfilePicture,
@@ -15,6 +16,12 @@ export default function Profile() {
       language: ['Français', 'Anglais'],
       address: '11 Avenue d\'Eysines, Bordeaux',
       activities: [3, 5, 6, 4],
+      description : `Lorem ipsum dolor sit amet consectetur. Massa ut ac amet tempor mi.
+                Porttitor neque cras lacus morbi cras tortor velit aliquam libero. 
+                Sapien arcu elit in consectetur arcu quam augue. Amet id elit arcu volutpat
+                adipiscing lorem erat in id. Suscipit imperdiet feugiat suspendisse sodales.
+                Magna orci proin laoreet vitae egestas leo varius. Egestas amet suspendisse
+                platea ante vitae sed vitae magna aenean. Pellentesque porttitor aliquam sit sit.`
   }
 
   return (
