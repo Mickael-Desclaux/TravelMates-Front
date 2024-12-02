4d4d87247c74afc1dc067f0a1b8ffeea9f70e4ee
EstherVlp <35601411+EstherVlp@users.noreply.github.com>
2024-12-02 13:52:34 +0100

    [TRA-238] Updated some elements to provide formik instead to useState and added the fields language and activity

--- travel-mates/src/pages/Profile/ProfileEdit.tsx
+++ travel-mates/src/pages/Profile/ProfileEdit.tsx
@@ -1,32 +1,27 @@
-import { useState, useRef } from "react";
-import { ProfileData } from "../../interfaces/ProfileInterface";
 import ProfilePicture from "../../assets/profile/profil1.jpg";
 import { NavLink } from "react-router-dom";
 import Arrow from "../../assets/icons/arrow.svg";
 import EditIcon from "../../assets/icons/edit-icon.svg";
+import { Typography } from "@material-tailwind/react";
+import { Field, Formik, Form } from "formik";
+import CustomSelect from "../../components/CustomSelect/CustomSelect";
+import ActivityPicker from "../../components/ActivityPicker/ActivityPicker";
+import { useRef } from "react";
 
 export default function ProfileEdit() {
-  // Fake profileEdit data
-  const [profile, setProfile] = useState<ProfileData>({
-    id: 1,
-    profilePicture: ProfilePicture,
-    firstName: "Éloïse",
-    lastName: "DeBordeaux",
-    age: 25,
-    gender: "Femme",
-    language: ["Français", "Anglais"],
-    address: "11 Avenue d'Eysines, Bordeaux",
-    activities: [3, 5, 6, 4],
-    description: `Lorem ipsum dolor sit amet consectetur. Massa ut ac amet tempor mi.
-                Porttitor neque cras lacus morbi cras tortor velit aliquam libero. 
-                Sapien arcu elit in consectetur arcu quam augue. Amet id elit arcu volutpat
-                adipiscing lorem erat in id. Suscipit imperdiet feugiat suspendisse sodales.
-                Magna orci proin laoreet vitae egestas leo varius. Egestas amet suspendisse
-                platea ante vitae sed vitae magna aenean. Pellentesque porttitor aliquam sit sit.`,
-  });
-
   // Reference to the hidden file input so it can be clicked through the profile picture
   const fileInputRef = useRef<HTMLInputElement | null>(null);
+
+  // Function to handle file input change
+  const handleFileChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    setFieldValue: (field: string, value: any) => void
+  ) => {
+    if (e.target.files) {
+      const file = URL.createObjectURL(e.target.files[0]);
+      setFieldValue("profilePicture", file);
+    }
+  };
 
   // Trigger file input on profile picture click
   const handleProfileClick = () => {
@@ -35,28 +30,24 @@ export default function ProfileEdit() {
     }
   };
 
-  // Update profile picture upon file selection
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files) {
-      const file = URL.createObjectURL(e.target.files[0]);
-      setProfile((prevProfile) => ({
-        ...prevProfile,
-        profilePicture: file,
-      }));
-    }
-  };
-
-  // Handle changes for editable fields for address and bio
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    const { name, value } = e.target;
-    setProfile((prevProfile) => ({
-      ...prevProfile,
-      [name]: value,
-    }));
-  };
+  // List of language options for the custom select dropdown of profile
+  const listOptionsLanguageProfile = [
+    "Allemand",
+    "Anglais",
+    "Arabe",
+    "Espagnol",
+    "Français",
+    "Grec",
+    "Italien",
+    "Mandarin",
+    "Néerlandais",
+    "Polonais",
+    "Portugais",
+    "Russe",
+  ];
 
   return (
-    <div className="container mx-auto p-4 md:mt-32">
+    <div className="container mx-auto mb-36 p-4 md:mt-32">
       <div className="flex">
         <NavLink
           to={"/profile"}
@@ -66,70 +57,174 @@ export default function ProfileEdit() {
           <img
             src={Arrow}
             alt="Croix de fermeture de la page"
-            className="w-10 h-10 md:w-8 md:h-8"
+            className="lg:w-12 lg:h-12 sm:w-10 sm:h-10"
           />
         </NavLink>
-        <h1 className="flex-1 text-center text-2xl font-title font-bold mb-4">
+        <Typography
+          variant="h1"
+          className="mb-4 flex-1 text-center text-2xl font-title"
+        >
           Modifier le profil
-        </h1>
+        </Typography>
       </div>
 
-      {/* Profile picture section with overlay for edit */}
-      <div
-        className="relative flex items-center mx-auto w-32 h-32 mb-4 rounded-full cursor-pointer"
-        onClick={handleProfileClick}
+      {/* Profile form */}
+      <Formik
+        initialValues={{
+          profilePicture: ProfilePicture,
+          firstName: "Éloïse",
+          lastName: "DeBordeaux",
+          age: 25,
+          gender: "Femme",
+          language: ["Français", "Anglais"],
+          address: "11 Avenue d'Eysines, Bordeaux",
+          activities: [3, 5, 6, 4],
+          description: `Lorem ipsum dolor sit amet consectetur. Massa ut ac amet tempor mi.
+                Porttitor neque cras lacus morbi cras tortor velit aliquam libero. 
+                Sapien arcu elit in consectetur arcu quam augue. Amet id elit arcu volutpat
+                adipiscing lorem erat in id. Suscipit imperdiet feugiat suspendisse sodales.
+                Magna orci proin laoreet vitae egestas leo varius. Egestas amet suspendisse
+                platea ante vitae sed vitae magna aenean. Pellentesque porttitor aliquam sit sit.`,
+        }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
       >
-        <input
-          type="file"
-          name="profilePicture"
-          className="hidden"
-          ref={fileInputRef}
-          onChange={handleFileChange}
-        />
-        <img
-          src={profile.profilePicture}
-          alt="Photo de profil"
-          className="w-full h-full rounded-full cursor-pointer"
-        />
-        <div className="absolute inset-0 bg-black bg-opacity-30 rounded-full flex items-center justify-center">
-          <img src={EditIcon} alt="Modifier" className="w-8 h-8 text-white" />
-        </div>
-      </div>
+        {({ values, handleChange, setFieldValue }) => (
+          <Form>
+            {/* Profile picture section with overlay for edit */}
+            <div
+              className="relative flex items-center mx-auto w-32 h-32 mb-4 rounded-full cursor-pointer"
+              onClick={handleProfileClick}
+            >
+              <input
+                type="file"
+                name="profilePicture"
+                className="hidden"
+                ref={fileInputRef}
+                onChange={(e) => handleFileChange(e, setFieldValue)}
+              />
+              <img
+                src={values.profilePicture}
+                alt="Photo de profil"
+                className="w-full h-full rounded-full cursor-pointer"
+              />
+              <div className="absolute inset-0 bg-black bg-opacity-30 hover:bg-opacity-50 rounded-full flex items-center justify-center">
+                <img
+                  src={EditIcon}
+                  alt="Modifier"
+                  className="w-8 h-8 text-white"
+                />
+              </div>
+            </div>
 
-      {/* Full name field */}
-      <div className="mb-6">
-        <label className="mb-2 block text-black font-bold">Nom et Prénom</label>
-        <input
-          type="text"
-          value={`${profile.firstName} ${profile.lastName}`}
-          disabled
-          className="w-full p-2 border rounded-md border-gray-300 text-gray-500"
-        />
-      </div>
+            {/* Firstname and lastname field */}
+            <div className="mb-6">
+              <label htmlFor="firstNameAndLastName">
+                <Typography
+                  variant="h6"
+                  className="mb-2 block text-black font-bold"
+                >
+                  Prénom et Nom
+                </Typography>
+              </label>
+              <input
+                type="text"
+                name="firstNameAndLastName"
+                id="firstNameAndLastName"
+                value={`${values.firstName} ${values.lastName}`}
+                disabled
+                className="w-full p-2 border rounded-md border-gray-300 text-gray-500"
+              />
+            </div>
 
-      {/* Address field */}
-      <div className="mb-6">
-        <label className="mb-2 block text-black font-bold">Adresse</label>
-        <input
-          type="text"
-          name="address"
-          value={profile.address}
-          onChange={handleChange}
-          className="w-full p-2 border rounded-md border-gray-300"
-        />
-      </div>
+            {/* Address field */}
+            <div className="mb-6">
+              <label htmlFor="address">
+                <Typography
+                  variant="h6"
+                  className="mb-2 block text-black font-bold"
+                >
+                  Adresse
+                </Typography>
+              </label>
+              <input
+                type="text"
+                name="address"
+                id="address"
+                value={values.address}
+                onChange={handleChange}
+                className="w-full p-2 border rounded-md border-gray-300"
+              />
+            </div>
 
-      {/* Biography field */}
-      <div className="mb-6">
-        <label className="mb-2 block text-black font-bold">Bio</label>
-        <textarea
-          name="description"
-          value={profile.description}
-          onChange={handleChange}
-          className="w-full p-2 border rounded-md border-gray-300"
-          rows={10}
-        />
-      </div>
+            {/* Biography field */}
+            <div className="mb-6">
+              <label htmlFor="biography">
+                <Typography
+                  variant="h6"
+                  className="mb-2 block text-black font-bold"
+                >
+                  Bio
+                </Typography>
+              </label>
+              <textarea
+                name="description"
+                id="biography"
+                value={values.description}
+                onChange={handleChange}
+                className="w-full p-2 border rounded-md border-gray-300"
+                rows={10}
+                placeholder="Votre biographie"
+                maxLength={1000}
+              />
+            </div>
+
+            {/* Activities field */}
+            <div className="my-8">
+              <label htmlFor="activities">
+                <Typography
+                  variant="h6"
+                  className="mb-2 block text-black font-bold"
+                >
+                  Activités
+                </Typography>
+              </label>
+              <ActivityPicker />
+            </div>
+
+            {/* Language field */}
+            <div className="mb-16 relative">
+              <label htmlFor="language">
+                <Typography
+                  variant="h6"
+                  className="mb-2 block text-black font-bold"
+                >
+                  Langue
+                </Typography>
+              </label>
+              <div className="relative z-10">
+                <Field
+                  name="language"
+                  id="language"
+                  component={CustomSelect}
+                  options={listOptionsLanguageProfile}
+                  multiple={true}
+                  className="max-h-60 overflow-y-auto"
+                />
+              </div>
+              <div className="mt-8">
+                <button
+                  type="submit"
+                  className="p-2 lg:w-1/3 w-3/4 bg-green text-white rounded hover:bg-opacity-85 mx-auto block"
+                >
+                  Valider les modifications
+                </button>
+              </div>
+            </div>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 }
